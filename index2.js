dce710a8d72daa80609d38521ad986e559d9c236
Sunay Devaliya <141119214+kevit-sunay-devaliya@users.noreply.github.com>
2023-09-05 15:41:30 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,5 +1,1 @@
-app.get('/', function (req, res) {
-    console.log("Got a GET request for the homepage");
-    const userCount = Math.ceil(Math.random() * 100);
-    res.send(`Hello GET. Users are: ${userCount}`);
- })
+
